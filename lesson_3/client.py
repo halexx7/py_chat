a2845aa99610e6f8ccc74c34ebcb1b9c8247601c
Aleksey Khlybov <halexx7@gmail.com>
2021-05-10 06:17:26 +0300

    Lesson_3. Add decision

---
+++ lesson_3/client.py
@@ -0,0 +1,38 @@
+from socket import *
+import pickle
+import sys
+import argparse
+
+def createParser ():
+    parser = argparse.ArgumentParser()
+    parser.add_argument ('addr', nargs='+', default='')
+    parser.add_argument ('port', nargs='?', default='7777')
+    return parser
+
+
+def presets_msg():
+    msg = {
+        "action": "authenticate",
+        "time": "<unix timestamp>",
+        "user": {
+                "account_name": "Maver1ck",
+                "password":     "CorrectHorseBatterStaple"
+        }
+    }
+    cli_sock.send(pickle.dumps(msg))
+    data = cli_sock.recv(1024)
+    print(f'Сообщение от сервера: {pickle.loads(data)}, длинной {len(data)} байт')
+
+
+if __name__ == "__main__":
+    # socket
+    cli_sock = socket(AF_INET, SOCK_STREAM)
+
+
+    # connect
+    parser = createParser()
+    namespace = parser.parse_args(sys.argv[1:])
+    cli_sock.connect((namespace.addr, namespace.port))     
+    print('Connected to remote host...')
+
+    presets_msg()
---
+++ lesson_3/server.py
@@ -0,0 +1,41 @@
+from socket import *
+import time
+import pickle
+import sys
+import argparse
+
+
+def createParser ():
+    parser = argparse.ArgumentParser()
+    parser.add_argument ('-p', '--port', default=7777)
+    parser.add_argument ('-a', '--addr', default='')
+    return parser
+
+
+def receiver():
+    while True:
+        client, addr = srv_sock.accept()
+        data = client.recv(1024)
+        print(pickle.loads(data))
+        response = {
+            "response": 200,
+            "alert":"ОК"
+        }
+        client.send(pickle.dumps(response))
+        client.close()
+
+
+if __name__ == "__main__":
+    # socket
+    srv_sock = socket(AF_INET, SOCK_STREAM)
+
+    # bind
+    parser = createParser()
+    namespace = parser.parse_args(sys.argv[1:])
+    srv_sock.bind((namespace.addr, namespace.port))
+
+    # listen    
+    srv_sock.listen(5)
+    print('Chat server started on port : ' + str(namespace.port))
+
+    receiver()
