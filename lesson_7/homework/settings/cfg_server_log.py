ebcdd7d7d2f598c6f7191a74e5762ab3ce2ae8f3
Alexey Khlybov <halexx7@gmail.com>
2021-05-24 14:30:02 +0300

    lesson_7. More, more code

---
+++ lesson_7/homework/settings/cfg_server_log.py
@@ -0,0 +1,19 @@
+import logging
+from logging.handlers import TimedRotatingFileHandler
+
+from settings.variables import ENCODING, LOG_FILENAME, LOGGER_NAME, WHEN_INTERVAL
+
+
+FILE_LOG_LVL = logging.INFO
+
+server_formatter = logging.Formatter(
+    f"%(asctime)-5s - %(levelname)-5s | %(module)-5s | %(message)s", datefmt="%Y-%m-%dT%H:%M:%S"
+)
+
+logHandler = TimedRotatingFileHandler(LOG_FILENAME, when=WHEN_INTERVAL, interval=1, encoding=ENCODING)
+logHandler.setFormatter(server_formatter)
+
+
+# logger.addHandler(logHandler)
+logger = logging.getLogger(LOGGER_NAME)
+logger.setLevel(FILE_LOG_LVL)
